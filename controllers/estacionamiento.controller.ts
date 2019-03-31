3f89d280774f2a918487e31ebec62e3dabef042f
Luis Eguia <jlewicho@gmail.com>
2019-03-31 01:40:44 -0600

    Finciónes para obtener estacionamiento.

--- controllers/estacionamiento.controller.ts
+++ controllers/estacionamiento.controller.ts
@@ -1,6 +1,35 @@
 import { Request, Response } from "express";
-import Estacionamiento from './../models/CajonEstacionamiento';
+import auto from "../models/Automovil";
+import cajon from "../models/CajonEstacionamiento";
+import { controlEstacionamiento } from '../libraries/control-estacionamiento';
 
-export const getEstacionamiento = (req: Request, res: Response) => {
-  
+export class estacionamientoController {
+    private control_estacionamiento: controlEstacionamiento = new controlEstacionamiento();
+
+    constructor() { }
+
+    getEstacionamiento(req: Request, res: Response) {
+        try {
+            let estacionamiento: cajon[] = this.control_estacionamiento.getEstacionamiento();
+
+            if (estacionamiento.length === 0) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'No se obtuvieron datos.'
+                    }
+                });
+            }
+
+            res.json({
+                ok: true,
+                estacionamiento
+            });
+        } catch (err) {
+            res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+    }
 }
--- routes/routes.ts
+++ routes/routes.ts
@@ -1,17 +1,18 @@
 import { Router } from 'express';
 import { automovilController } from './../controllers/automovil.controller'
-import * as estacionamiento from './../controllers/estacionamiento.controller';
+import { estacionamientoController } from './../controllers/estacionamiento.controller';
 
 const router = Router();
 const automovil = new automovilController();
+const estacionamiento = new estacionamientoController();
 
 router.get('/', (req, res) => {
     res.end('API rest por Luis Eguia.');
 });
 
 router.get('/getAutos', (req, res) => automovil.getAutomoviles(req, res));
-router.post('/addAuto', (req, res) => automovil.addAutomovil(req, res))
+router.post('/addAuto', (req, res) => automovil.addAutomovil(req, res));
 
-// router.get('/getEstacionamiento', (req, res) => estacionamiento.getEstacionamiento(req, res));
+router.get('/getEstacionamiento', (req, res) => estacionamiento.getEstacionamiento(req, res));
 
 export default router;
